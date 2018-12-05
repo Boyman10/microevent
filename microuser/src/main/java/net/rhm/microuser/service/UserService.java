5c794be1e07c515ee25847e615fe4d2baa1dbce0
Boyman10 <contact@yasi.fr>
2018-12-05 21:36:23 +0100

    organizing things around kafka and user registering method

--- microuser/src/main/java/net/rhm/microuser/service/UserService.java
+++ microuser/src/main/java/net/rhm/microuser/service/UserService.java
@@ -1,7 +1,9 @@
 package net.rhm.microuser.service;
 
 import net.rhm.microuser.entity.User;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface UserService {
     User registerUser(User input);
     Iterable<User> findAll();
