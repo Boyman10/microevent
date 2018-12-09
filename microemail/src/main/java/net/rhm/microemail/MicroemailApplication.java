0450f2160b343421c071db67246652b7ae95de5b
Boyman10 <contact@yasi.fr>
2018-12-09 16:56:58 +0100

    Done with configuration and email app parameters - now testing kafka...

--- microemail/src/main/java/net/rhm/microemail/MicroemailApplication.java
+++ microemail/src/main/java/net/rhm/microemail/MicroemailApplication.java
@@ -2,8 +2,10 @@ package net.rhm.microemail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MicroemailApplication {
 
     public static void main(String[] args) {
