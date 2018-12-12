b0d286ec83706b2c6b0d8f2a1c63714918bb980f
Boyman10 <contact@yasi.fr>
2018-12-12 21:23:53 +0100

    Updating Group id - see if kafka crashes...

--- microemail/src/main/java/net/rhm/microemail/config/ReceiverConfig.java
+++ microemail/src/main/java/net/rhm/microemail/config/ReceiverConfig.java
@@ -31,7 +31,7 @@ public class ReceiverConfig {
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "UserCreatedConsumer");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "microemail_consumer");
 
         return props;
     }
