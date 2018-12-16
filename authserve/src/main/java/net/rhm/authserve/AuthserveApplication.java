79f79cbeeae8b55bcf72d2de714ead1aec59a590
Boyman10 <contact@yasi.fr>
2018-12-16 18:49:53 +0100

    onto building an auth2 authentication server for our micro services app

---
+++ authserve/src/main/java/net/rhm/authserve/AuthserveApplication.java
@@ -0,0 +1,14 @@
+package net.rhm.authserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthserveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthserveApplication.class, args);
+    }
+
+}
+
