1fea689ff630f55dd35e8d3dd806a04c8b4a24c6
Boyman10 <contact@yasi.fr>
2018-12-16 18:18:22 +0100

    Initializing zuul app for all our services referenced via eureka - testing on users

---
+++ microgate/src/main/java/net/rhm/microgate/MicrogateApplication.java
@@ -0,0 +1,18 @@
+package net.rhm.microgate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class MicrogateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicrogateApplication.class, args);
+    }
+
+}
+
