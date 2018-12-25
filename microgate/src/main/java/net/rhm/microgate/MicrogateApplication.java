a601879f424b1cdecc3dae426abbf40eb4af3b04
Boyman10 <contact@yasi.fr>
2018-12-25 22:34:44 +0100

    On zuul enabling SSO using Keycloak...

--- microgate/src/main/java/net/rhm/microgate/MicrogateApplication.java
+++ microgate/src/main/java/net/rhm/microgate/MicrogateApplication.java
@@ -2,12 +2,17 @@ package net.rhm.microgate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+/**
+ * See https://www.baeldung.com/spring-security-oauth2-enable-resource-server-vs-enable-oauth2-sso
+ */
 @SpringBootApplication
 @EnableEurekaClient
 @EnableZuulProxy
+@EnableOAuth2Sso
 public class MicrogateApplication {
 
     public static void main(String[] args) {
