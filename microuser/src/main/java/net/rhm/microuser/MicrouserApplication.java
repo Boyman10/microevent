25bc52d93a8e68551ecefeb5fb65293b0d46ee2a
Boyman10 <contact@yasi.fr>
2018-12-30 20:16:01 +0100

    Onto securing other apps.

--- microuser/src/main/java/net/rhm/microuser/MicrouserApplication.java
+++ microuser/src/main/java/net/rhm/microuser/MicrouserApplication.java
@@ -2,8 +2,10 @@ package net.rhm.microuser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @SpringBootApplication
+@EnableOAuth2Sso
 public class MicrouserApplication {
 
     public static void main(String[] args) {
