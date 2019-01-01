6525de9bf8918a9edf1c9d52b9ee8e3834e497bd
Boyman10 <contact@yasi.fr>
2019-01-01 18:56:50 +0100

    Testing next

--- microuser/src/test/java/net/rhm/microuser/MicrouserApplicationTests.java
+++ microuser/src/test/java/net/rhm/microuser/MicrouserApplicationTests.java
@@ -5,6 +5,10 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+/**
+ * Todo next :
+ * @see https://www.baeldung.com/oauth-api-testing-with-spring-mvc
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MicrouserApplicationTests {
