dbaf3511d32d1d1f4a050d030a01b31d145e1dcd
Boyman10 <contact@yasi.fr>
2019-01-01 19:54:27 +0100

    Testing @RequestHeader to print out the headers while posting  data !

--- microuser/src/main/java/net/rhm/microuser/controller/UserController.java
+++ microuser/src/main/java/net/rhm/microuser/controller/UserController.java
@@ -4,13 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import net.rhm.microuser.entity.User;
 import net.rhm.microuser.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -26,15 +24,17 @@ public class UserController {
 
     //@PreAuthorize("hasAuthority('ROLE_USER')")
     @RequestMapping(method = RequestMethod.GET, path = "/member")
-    public ResponseEntity<Iterable<User>> getAll() {
+    public ResponseEntity<Iterable<User>> getAll(@RequestHeader(value="User-Agent") String userAgent) {
         Iterable<User> all = userService.findAll();
-        log.info("Hello me !!");
+        log.info("Hello me !!" + userAgent);
         return new ResponseEntity<>(all, HttpStatus.OK);
     }
 
     @RequestMapping(method = RequestMethod.POST, path = "/member")
-    public ResponseEntity<User> register(@RequestBody User input) {
+    public ResponseEntity<User> register(@RequestBody User input, @RequestHeader HttpHeaders headers) {
         User result = userService.registerUser(input);
+        log.info("Hello me !!" + headers);
+
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 }
