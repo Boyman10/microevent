c2db35e7767cf57e4367e46e0b084e0d82fedd11
Boyman10 <contact@yasi.fr>
2019-01-06 15:58:38 +0100

    Added context-path to micro user app to match the one defined within zuul app path

--- microgate/src/main/java/net/rhm/microgate/ResourceServerConfig.java
+++ microgate/src/main/java/net/rhm/microgate/ResourceServerConfig.java
@@ -1,13 +1,16 @@
 package net.rhm.microgate;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.web.util.matcher.RequestHeaderRequestMatcher;
 
 @Configuration
 @EnableResourceServer
+@Slf4j
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Override
@@ -17,4 +20,6 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/**").authenticated();
     }
+
+
 }
