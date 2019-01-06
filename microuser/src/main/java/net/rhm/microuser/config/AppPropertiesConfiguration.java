e2d23cab5becef548db1457be399a9520052a1bc
Boyman10 <contact@yasi.fr>
2019-01-06 17:06:15 +0100

    adding Sleuth to trace our micro app - dep and config

--- microuser/src/main/java/net/rhm/microuser/config/AppPropertiesConfiguration.java
+++ microuser/src/main/java/net/rhm/microuser/config/AppPropertiesConfiguration.java
@@ -15,6 +15,11 @@ public class AppPropertiesConfiguration {
     // testted string
     private String str;
 
+    public String getStr() {
+        return str;
+    }
 
-
+    public void setStr(String str) {
+        this.str = str;
+    }
 }
